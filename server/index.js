89f21a81b2bdcf2ab91b676f50874cdf589813d0
Himanshu <hv65616@gmail.com>
2024-03-02 18:44:09 +0530

    importing user routes

--- server/index.js
+++ server/index.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
-const dotenv = require("dotenv");
+const userRoutes = require("./routes/userRoute");
 
+const dotenv = require("dotenv");
 dotenv.config({ path: "./config.env" });
-const app = express();
+
 const port = process.env.PORT || 8081;
 const mongourl = process.env.MONGO_URL;
+const app = express();
+
 app.use(cors());
 app.use(express.json());
 
@@ -21,6 +24,8 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+
+app.use("/api/auth", userRoutes);
 
 app.listen(port, (req, res) => {
   console.log(`Server is running on ${port}`);
