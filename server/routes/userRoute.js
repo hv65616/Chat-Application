28942c768e9f405cb8afcee947c6b9e54fe582c0
Himanshu <hv65616@gmail.com>
2024-03-05 21:33:50 +0530

    backend route

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -5,4 +5,5 @@ const userController = require("../controllers/userController")
 const router = express.Router();
 router.post("/register", userController.register);
 router.post("/login", userController.login);
+router.post("/setAvatar/:id",userController.setAvatar)
 module.exports = router
