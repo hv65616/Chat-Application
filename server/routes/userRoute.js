3601a16ee62117e8ddc3467bb4089452a81daa3f
Himanshu <hv65616@gmail.com>
2024-03-07 15:35:19 +0530

    all user route

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,9 +1,10 @@
 const express = require("express");
 // const register = require("../controllers/userController");
 // const login = require("../controllers/userController")
-const userController = require("../controllers/userController")
+const userController = require("../controllers/userController");
 const router = express.Router();
 router.post("/register", userController.register);
 router.post("/login", userController.login);
-router.post("/setAvatar/:id",userController.setAvatar)
-module.exports = router
+router.post("/setAvatar/:id", userController.setAvatar);
+router.get("/allusers/:id", userController.allUsers);
+module.exports = router;
