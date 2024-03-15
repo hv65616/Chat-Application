41afc6f579ecbfa7abd5efdc8784eb6102a119bc
Himanshu <hv65616@gmail.com>
2024-03-16 01:40:19 +0530

    message route

--- client/src/utils/apiRoutes.js
+++ client/src/utils/apiRoutes.js
@@ -3,3 +3,5 @@ export const registerRoute = `${host}/api/auth/register`
 export const loginRoute = `${host}/api/auth/login`;
 export const setAvatarRoute = `${host}/api/auth/setAvatar`;
 export const allUserRoute = `${host}/api/auth/allusers`;
+export const sendMessageRoute = `${host}/api/message/addmessage`;
+export const getAllMessageRoute = `${host}/api/message/getmessage`;
