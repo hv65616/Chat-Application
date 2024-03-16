300a885e4eaae81bc61ff8c9912a60feeddaf610
Himanshu <hv65616@gmail.com>
2024-03-16 23:00:50 +0530

    socket implement

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const socket = require("socket.io");
 const userRoutes = require("./routes/userRoute");
 const messageRoutes = require("./routes/messagesRoute");
 const dotenv = require("dotenv");
@@ -28,6 +29,26 @@ mongoose
 app.use("/api/auth", userRoutes);
 app.use("/api/message", messageRoutes);
 
-app.listen(port, (req, res) => {
+const server = app.listen(port, (req, res) => {
   console.log(`Server is running on ${port}`);
+});
+
+const io = socket(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    credentials: true,
+  },
+});
+global.onlineUsers = new Map();
+io.on("connection", (socket) => {
+  global.chatSocket = socket;
+  socket.on("add-user", (userId) => {
+    onlineUsers.set(userId, socket.id);
+  });
+  socket.on("send-msg", (data) => {
+    const sendUserSocket = onlineUsers.get(data.to);
+    if (sendUserSocket) {
+      socket.to(sendUserSocket).emit("msg-recieve", data.message);
+    }
+  });
 });
