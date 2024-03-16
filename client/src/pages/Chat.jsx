4e915a1cc8a8fd8f425a7b32772125435ca32c15
Himanshu <hv65616@gmail.com>
2024-03-16 23:02:07 +0530

    adding user to socket

--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -1,12 +1,14 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import styled from "styled-components";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import { allUserRoute } from "../utils/apiRoutes";
+import { allUserRoute, host } from "../utils/apiRoutes";
 import Contacts from "../components/Contacts";
 import Welcome from "../components/Welcome";
 import ChatContainer from "../components/ChatContainer";
+import { io } from "socket.io-client";
 function Chat() {
+  const socket = useRef();
   const [contacts, setContacts] = useState([]);
   const [currentUser, setCurrentUser] = useState(undefined);
   const [currentChat, setCurrentChat] = useState(undefined);
@@ -23,6 +25,12 @@ function Chat() {
     };
     getCurrentUser();
   }, [navigate]);
+  useEffect(() => {
+    if (currentUser) {
+      socket.current = io(host);
+      socket.current.emit("add-user", currentUser._id);
+    }
+  }, [currentUser]);
   useEffect(() => {
     const user = async () => {
       if (currentUser) {
@@ -56,6 +64,7 @@ function Chat() {
             <ChatContainer
               currentChat={currentChat}
               currentUser={currentUser}
+              socket={socket}
             ></ChatContainer>
           )}
         </div>
