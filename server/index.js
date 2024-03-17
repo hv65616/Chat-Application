56793a34d3d9b22417b225531b643eb122cfec98
Himanshu <hv65616@gmail.com>
2024-03-17 15:59:33 +0530

    try1 resolving cors issue

--- server/index.js
+++ server/index.js
@@ -10,7 +10,12 @@ dotenv.config({ path: "./config.env" });
 const port = process.env.PORT || 8081;
 const mongourl = process.env.MONGO_URL;
 const app = express();
-
+const corsOptions = {
+  origin: "https://bingle.onrender.com",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 app.use(cors());
 app.use(express.json());
 
@@ -36,6 +41,8 @@ const server = app.listen(port, (req, res) => {
 const io = socket(server, {
   cors: {
     origin: "https://bingle.onrender.com/",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["my-custom-header"],
     credentials: true,
   },
 });
